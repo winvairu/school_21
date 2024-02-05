63fa12e8cf5f4906f1dce5fa3750073f4a9465e9
winvairu <winvairu@outlook.com>
2024-02-05 07:33:11 -0800

    add quest3.sh

---
+++ T01D01-0-master/src/quest3.sh
@@ -0,0 +1,9 @@
+chmod +x ai_door_management_module.sh
+mv door_management_fi door_management_files
+mkdir door_configuration
+mkdir door_map
+mkdir door_logs
+mv *.log door_logs
+mv *.conf door_configuration
+mv door_map_1.1 door_map
+chmod +x ai_door_control.sh
