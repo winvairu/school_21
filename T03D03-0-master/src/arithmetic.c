09aee80890357ab6bf934d3c96e954384fe45cb9
winvairu <winvairu@outlook.com>
2024-02-09 08:41:08 -0800

    edit a file arithmetic.c

--- T03D03-0-master/src/arithmetic.c
+++ T03D03-0-master/src/arithmetic.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int dif(int a, int b);
+int mul(int a, int b);
+int quo(int a, int b);
 
-int main()
-{
+int main() {
+    int x, y;
+    if (scanf("%d%d", &x, &y) == 2) {
+        if ((y > 0) || (y < 0)) {
+            printf("%d %d %d %d\n", sum(x, y), dif(x, y), mul(x, y), quo(x, y));
+        } else {
+            printf("%d %d %d n/a\n", sum(x, y), dif(x, y), mul(x, y));
+        }
+    } else {
+        printf("n/a\n");
+    }
     return 0;
 }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
-}
+int sum(int a, int b) { return a + b; }
+int dif(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+int quo(int a, int b) { return a / b; }
